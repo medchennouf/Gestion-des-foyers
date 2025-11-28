25706d9301c27e236220eefd0676d0679e505a8a
Mohamed <mohamed.chennouf@esprit.tn>
2025-11-28 08:44:30 +0000

    Test Docker Hub push

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevSecOps – Gestion des foyers (Docker + Jenkins + Trivy + Gitleaks)"
+    return "DevSecOps – Gestion des foyers (Docker + Jenkins + Trivy + Gitleaks + ZAP)"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
